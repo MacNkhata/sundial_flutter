e706884186ccbe37aaa45f809b7b352c8d5c418f
Macdonald Nkhata <nkhatamac27@gmail.com>
2024-06-29 14:01:45 +0200

    finished the loading page

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -29,25 +30,59 @@ class _LoadingScreenState extends State<LoadingScreen> {
       title: title,
       theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
       home: Scaffold(
+        backgroundColor: Color(0xFF06141B),
         body: SafeArea(
-          child: Column(
-            children: [
-              const Padding(padding: EdgeInsets.fromLTRB(16, 16, 16, 0)),
-              Center(
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  width: 120,
-                  height: 122.64,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Text(
-                'Let\'s burn some fat.',
-                style: GoogleFonts.poppins(),
-              )
-            ],
+            child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/loading_bg.png'),
+                fit: BoxFit.fitWidth),
           ),
-        ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Spacer(),
+                Center(
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 120,
+                    height: 122.64,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const Spacer(),
+                Text(
+                  'Let\'s \nburn some fat.',
+                  // textAlign: TextAlign.left,
+                  style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.w200,
+                      fontSize: 42.0,
+                      color: Colors.white,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                ),
+                Text(
+                  'We both know you kinda fat so leave the bullshitting aside let\'s get in shape.',
+                  style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                          fontWeight: FontWeight.w200,
+                          fontSize: 16.0,
+                          color: Colors.white)),
+                ),
+                const Spacer(),
+                Center(
+                  child: LoadingAnimationWidget.discreteCircle(
+                      color: Color(0XFFF45050), size: 42),
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+        )),
       ),
     );
   }
