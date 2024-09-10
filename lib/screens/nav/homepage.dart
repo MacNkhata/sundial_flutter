dea48e29dc6cee096380558c458fa5b583d4d716
Macdonald Nkhata <nkhatamac27@gmail.com>
2024-09-10 14:22:50 +0200

    finished the static homepage

--- lib/screens/nav/homepage.dart
+++ lib/screens/nav/homepage.dart
@@ -35,123 +35,51 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
       backgroundColor: const Color(0xFF06141B),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      greetingMessage,
-                      style: GoogleFonts.poppins(
-                        textStyle: const TextStyle(
-                            fontSize: 16.0, color: Colors.white70),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        greetingMessage,
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                              fontSize: 16.0, color: Colors.white70),
+                        ),
                       ),
-                    ),
-                    Text(
-                      "Username",
-                      style: GoogleFonts.poppins(
-                        textStyle: const TextStyle(
-                            fontSize: 24.0, color: Color(0XFFF45050)),
-                      ),
-                    )
-                  ],
-                ),
-                const Spacer(),
-                const IconButton(
-                  icon: HugeIcon(
-                    icon: HugeIcons.strokeRoundedSettings05,
-                    color: Color(0XFFF45050),
-                    size: 32.0,
+                      Text(
+                        "Username",
+                        style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                              fontSize: 24.0, color: Color(0XFFF45050)),
+                        ),
+                      )
+                    ],
                   ),
-                  onPressed: null,
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0XFF942D2D),
-                          Color(0XFFF45050),
-                          Color(0XFFF45050),
-                        ],
-                        begin: Alignment.bottomLeft,
-                        end: Alignment.topRight,
-                      ),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(12.0),
-                      ),
+                  const IconButton(
+                    icon: HugeIcon(
+                      icon: HugeIcons.strokeRoundedSettings05,
+                      color: Color(0XFFF45050),
+                      size: 32.0,
                     ),
-                    height: 160,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Minutes \nMoved",
-                                style: GoogleFonts.poppins(
-                                    textStyle: const TextStyle(
-                                        fontSize: 18.0,
-                                        color: Color(0xFFF0F0F0))),
-                              ),
-                              const Spacer(),
-                              const HugeIcon(
-                                icon: HugeIcons.strokeRoundedStopWatch,
-                                color: Color(0xFFF0F0F0),
-                                size: 32.0,
-                              ),
-                            ],
-                          ),
-                          const Spacer(),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                "120",
-                                style: GoogleFonts.poppins(
-                                  textStyle: const TextStyle(
-                                      fontSize: 54.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xFFF0F0F0),
-                                      height: .7),
-                                ),
-                              ),
-                              Text(
-                                "min",
-                                style: GoogleFonts.poppins(
-                                  textStyle: const TextStyle(
-                                      fontSize: 20.0,
-                                      color: Color(0xFFF0F0F0),
-                                      height: 1.0),
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 16.0,
-                ),
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
+                    onPressed: null,
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
                         gradient: LinearGradient(
                           colors: [
                             Color(0XFF942D2D),
@@ -161,66 +89,231 @@ class _HomepageState extends State<Homepage> {
                           begin: Alignment.bottomLeft,
                           end: Alignment.topRight,
                         ),
-                        borderRadius: BorderRadius.all(Radius.circular(12.0))),
-                    height: 160,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Burned \nCalories",
-                                style: GoogleFonts.poppins(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(12.0),
+                        ),
+                      ),
+                      height: 160,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Minutes \nMoved",
+                                  style: GoogleFonts.poppins(
+                                      textStyle: const TextStyle(
+                                          fontSize: 18.0,
+                                          color: Color(0xFFF0F0F0))),
+                                ),
+                                const Spacer(),
+                                const HugeIcon(
+                                  icon: HugeIcons.strokeRoundedStopWatch,
+                                  color: Color(0xFFF0F0F0),
+                                  size: 32.0,
+                                ),
+                              ],
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  "120",
+                                  style: GoogleFonts.poppins(
                                     textStyle: const TextStyle(
-                                        fontSize: 18.0,
-                                        color: Color(0xFFF0F0F0))),
-                              ),
-                              const Spacer(),
-                              const HugeIcon(
-                                icon: HugeIcons.strokeRoundedFire,
-                                color: Color(0xFFF0F0F0),
-                                size: 32.0,
-                              ),
-                            ],
-                          ),
-                          const Spacer(),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                "144",
-                                style: GoogleFonts.poppins(
-                                  textStyle: const TextStyle(
-                                      fontSize: 54.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xFFF0F0F0),
-                                      height: .7),
+                                        fontSize: 54.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFFF0F0F0),
+                                        height: .7),
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                "kcal",
-                                style: GoogleFonts.poppins(
-                                  textStyle: const TextStyle(
-                                      fontSize: 20.0,
-                                      color: Color(0xFFF0F0F0),
-                                      height: 1.0),
+                                Text(
+                                  "min",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: const TextStyle(
+                                        fontSize: 20.0,
+                                        color: Color(0xFFF0F0F0),
+                                        height: 1.0),
+                                  ),
                                 ),
-                              ),
-                            ],
-                          )
-                        ],
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                )
-              ],
-            )
-          ],
+                  const SizedBox(
+                    width: 16.0,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0XFF942D2D),
+                              Color(0XFFF45050),
+                              Color(0XFFF45050),
+                            ],
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight,
+                          ),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(12.0))),
+                      height: 160,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Burned \nCalories",
+                                  style: GoogleFonts.poppins(
+                                      textStyle: const TextStyle(
+                                          fontSize: 18.0,
+                                          color: Color(0xFFF0F0F0))),
+                                ),
+                                const Spacer(),
+                                const HugeIcon(
+                                  icon: HugeIcons.strokeRoundedFire,
+                                  color: Color(0xFFF0F0F0),
+                                  size: 32.0,
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  "144",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: const TextStyle(
+                                        fontSize: 54.0,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(0xFFF0F0F0),
+                                        height: .7),
+                                  ),
+                                ),
+                                Text(
+                                  "kcal",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: const TextStyle(
+                                        fontSize: 20.0,
+                                        color: Color(0xFFF0F0F0),
+                                        height: 1.0),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 32.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Available Workouts",
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 18.0,
+                        color: Color(0xFFF0F0F0),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: null,
+                    child: Text(
+                      "View All",
+                      style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0XFFF45050)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 32.0,
+              ),
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 2,
+                          color: const Color(0XFFF45050),
+                        ),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(12),
+                        ),
+                      ),
+                      height: 80.0,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const HugeIcon(
+                              icon: HugeIcons.strokeRoundedFire,
+                              color: Color(0xFFF45050),
+                              size: 32.0,
+                            ),
+                            Text(
+                              "Workout Name",
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                  fontSize: 18.0,
+                                  color: Color(0xFFF0F0F0),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "60:00",
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFFF0F0F0),
+                                ),
+                              ),
+                            ),
+                            const IconButton(
+                                onPressed: null,
+                                icon: HugeIcon(
+                                    icon: HugeIcons
+                                        .strokeRoundedMoreVerticalSquare01,
+                                    color: Color(0xFFF45050)))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
